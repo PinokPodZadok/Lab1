e5774452a6ff85bbdbdb31b16af73cbccf4a8f10
PinokPodZadok <itutk@mail.ru>
2015-09-04 11:03:59 +0500

    Update lab3.java

--- lab3.java
+++ lab3.java
@@ -2,21 +2,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
  
-public class LB33
+public class LB3
 {
     public static void main(String[] args)
     {
-        ArrayList<String> arrlst = new ArrayList<String>();
+        ArrayList<String> arrlst = new ArrayList<String>();//Создаем массив
         @SuppressWarnings("resource")
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);//Создаем сканер
         
-        System.out.println("Введите комбинацию чисел от -бесконечности до +бесконечности:");
-        String[] line = sc.nextLine().split(" ");
-        for (int i = 0; i < line.length; ++i)
+        System.out.println("Введите комбинацию чисел от -бесконечности до +бесконечности:");//Приглашаем ввести числа
+        String[] line = sc.nextLine().split(" ");//Разделяем цифры
+        for (int i = 0; i < line.length; ++i)//Создаем цикл
             arrlst.add(line[i]);
-        System.out.println("Исходный массив: " + arrlst);
-        Collections.sort(arrlst, Collections.reverseOrder());
+        System.out.println("Исходный массив: " + arrlst); //Выводим введеный текст
+        Collections.sort(arrlst, Collections.reverseOrder());//Сортируем
         
-        System.out.println("Результат: " + arrlst);
+        System.out.println("Результат: " + arrlst);//Выводим отсортированый текст
     }
 }
