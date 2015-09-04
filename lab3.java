46633c36a075fad2bdd5675cbf0d432798fbe7ac
PinokPodZadok <itutk@mail.ru>
2015-09-04 16:12:42 +0500

    Update lab3.java

--- lab3.java
+++ lab3.java
@@ -1,22 +1,35 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
- 
-public class LB3
-{
-    public static void main(String[] args)
-    {
-        ArrayList<String> arrlst = new ArrayList<String>();//Создаем массив
-        @SuppressWarnings("resource")
-		Scanner sc = new Scanner(System.in);//Создаем сканер
-        
-        System.out.println("Введите комбинацию чисел от -бесконечности до +бесконечности:");//Приглашаем ввести числа
-        String[] line = sc.nextLine().split(" ");//Разделяем цифры
-        for (int i = 0; i < line.length; ++i)//Создаем цикл
-            arrlst.add(line[i]);
-        System.out.println("Исходный массив: " + arrlst); //Выводим введеный текст
-        Collections.sort(arrlst, Collections.reverseOrder());//Сортируем
-        
-        System.out.println("Результат: " + arrlst);//Выводим отсортированый текст
-    }
+import java.lang.NumberFormatException;
+
+public class LB3 {
+	public static void main(String[] args) {
+		ArrayList<Integer> arrlst = new ArrayList<Integer>();// Создаем массив
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);// Создаем сканер
+
+		System.out
+				.println("Введите комбинацию ЧИСЕЛ от -бесконечности до +бесконечности:");// Приглашаем
+													// ввести
+													// числа
+		String[] line = sc.nextLine().split(" ");// Разделяем цифры
+		int i = 0; // обнуляем переменную i
+		try { // Пока
+			for (i = 0; i < line.length; ++i)
+				// Создаем цикл
+				arrlst.add(Integer.parseInt(line[i])); // Записываем цифры
+		} catch (NumberFormatException e) { // Если не цифра
+			System.out.println("Неверный формат данных: " + line[i]); // Тогда
+										// выводим
+										// предупреждение
+			return; // возвр.
+		}
+		System.out.println("Исходный массив: " + arrlst); // Выводим введеный
+								// текст
+		Collections.sort(arrlst, Collections.reverseOrder()); // Сортируем
+
+		System.out.println("Результат: " + arrlst);// Выводим отсортированый
+							// текст
+	}
 }
