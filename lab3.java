3f2d724ab83807efb547f1bc6a0546e589f3fda1
PinokPodZadok <itutk@mail.ru>
2015-09-04 10:33:37 +0500

    Update lab3.java

--- lab3.java
+++ lab3.java
@@ -1,30 +1,22 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
-
-public class LB3 {
-
-	public static void main(String[] args) {
-		// create array list object
-		ArrayList<String> arrlst = new ArrayList<String>();
-		// populate the list
-		System.out
-				.print("Введите комбинацию цифр от минус бесконечности до плюс бесконечности: ");
-		@SuppressWarnings("resource")
-		Scanner s = new Scanner(System.in);// Создаем сканер
-		String str = s.nextLine();
-		int c = 0;
-		for (int i = 0; i < str.length(); i++) {
-
-			c = str.codePointAt(i);
-			if (c == 32) {
-			} else  {
-				arrlst.add(str);
-			}
-		}
-
-		System.out.println("Вы ввели комбинацию :" + arrlst);
-		Collections.sort(arrlst, Collections.reverseOrder());
-		System.out.println("Отсортированная комбинация :" + arrlst);
-	}
+ 
+public class LB33
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> arrlst = new ArrayList<String>();
+        @SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Введите комбинацию чисел от -бесконечности до +бесконечности:");
+        String[] line = sc.nextLine().split(" ");
+        for (int i = 0; i < line.length; ++i)
+            arrlst.add(line[i]);
+        System.out.println("Исходный массив: " + arrlst);
+        Collections.sort(arrlst, Collections.reverseOrder());
+        
+        System.out.println("Результат: " + arrlst);
+    }
 }
